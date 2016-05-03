79d5d71b48f16c796a07c1162c84735a73ace4db
Shahaf Abileah <shahaf.abileah@airbnb.com>
2016-05-02 23:28:15 -0700

    Initial commit of tip calculator. Includes main screen and settings screen.

---
+++ tips/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  tips
+//
+//  Created by Shahaf Abileah on 5/2/16.
+//  Copyright © 2016 Shahaf Abileah. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+}
