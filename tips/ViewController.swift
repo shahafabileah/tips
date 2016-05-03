cb9ebd1839780fabd5625e851d6b2abe4ba182e1
Shahaf Abileah <shahaf.abileah@airbnb.com>
2016-05-02 19:47:32 -0700

    Initial Commit

---
+++ tips/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tips
+//
+//  Created by Shahaf Abileah on 5/2/16.
+//  Copyright © 2016 Shahaf Abileah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
